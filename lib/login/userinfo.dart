3484a316d2c08dca445c2d852651e692416adbcf
SUPAKORN HANPRASERT <6431503059@lamduan.mfu.ac.th>
2024-03-23 08:59:30 +0700

    login&registerFullFunction

---
+++ lib/login/userinfo.dart
@@ -0,0 +1,91 @@
+// userinfo.dart
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mealmentor/home.dart'; // Import HomePage
+
+class UserInfoPage extends StatefulWidget {
+  final String userId;
+  final String email;
+
+  UserInfoPage({required this.userId, required this.email});
+
+  @override
+  _UserInfoPageState createState() => _UserInfoPageState();
+}
+
+class _UserInfoPageState extends State<UserInfoPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _dobController = TextEditingController();
+  int _totalCalories = 0;
+
+  void _addUserInfoToFirestore(BuildContext context) {
+    // Get a reference to the Firestore collection
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    // Add user information to Firestore
+    users.doc(widget.userId).set({
+      'username': _usernameController.text.trim(),
+      'dob': _dobController.text.trim(),
+      'totalCalories': _totalCalories,
+      'email': widget.email, // Use email passed from RegisterScreen
+    }).then((value) {
+      // Navigate to the HomePage after adding user info
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      );
+    }).catchError((error) {
+      print('Failed to add user information: $error');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Information'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _dobController,
+              decoration: InputDecoration(
+                labelText: 'Date of Birth (YYYY-MM-DD)',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Total Calories',
+              ),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  _totalCalories = int.tryParse(value) ?? 0;
+                });
+              },
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () => _addUserInfoToFirestore(context),
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mealmentor/login/welcome.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: WelcomeScreen(),
     );
   }
 }
